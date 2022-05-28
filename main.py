916a0ba92f1de29a63f494155ab15a879f01f6b7
lee <derrosilee@gmail.com>
2022-05-28 22:30:12 +0300

    added ability for user to pick number range

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 # Number Guessing Game
-def print_hi(name):
-    print(f'Hi, {name}')
+
+import random  # will be used to generate the random numbers
+
+
+def index():  # will add function
+    number_range1 = int(input("Enter 1st range number: \t"))
+    number_range2 = int(input("Enter 2nd range number \t"))
+    generated_number = random.randint(number_range1, number_range2)
+    print(f"The Generated Number is {generated_number}")
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-
+    index()
