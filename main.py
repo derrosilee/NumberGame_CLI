fde48e6cb201199a576a8fc6811c1130e96be727
lee <derrosilee@gmail.com>
2022-05-29 21:30:40 +0300

    added basic number theory

--- main.py
+++ main.py
@@ -3,12 +3,22 @@
 import random  # will be used to generate the random numbers
 
 
-def index():  # will add function
+def number_generator():  # will add function
+    # To take users input on desired number range
     number_range1 = int(input("Enter 1st range number: \t"))
     number_range2 = int(input("Enter 2nd range number \t"))
+    # Generating the number based on user input
     generated_number = random.randint(number_range1, number_range2)
-    print(f"The Generated Number is {generated_number}")
+    print(f"You have selected number range {number_range1} - {number_range2}")
+    # Capturing users input on guessed Number
+    answer = int(input("Enter First Guess: "))
+    # Condition Checking
+    if answer == generated_number:
+        print(f"Congratulations u won the number is {generated_number}")
+    else:
+        print("Naaa That ain't it try again")
+    return number_range1, number_range2, generated_number
 
 
 if __name__ == '__main__':
-    index()
+    number_generator()
