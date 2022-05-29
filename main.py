c1fdfc19b76740b97034262c3d67c6c7702b9b13
lee <derrosilee@gmail.com>
2022-05-29 21:56:24 +0300

    added welcome message

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 # Number Guessing Game
+# Need to add a life counter to the game
 
 import random  # will be used to generate the random numbers
 
 
-def number_generator():  # will add function
+def number_generator():
+    print("Hello Welcome to the number guessing game,"
+          "Hope u Enjoy :)"
+          )
     # To take users input on desired number range
     number_range1 = int(input("Enter 1st range number: \t"))
     number_range2 = int(input("Enter 2nd range number \t"))
@@ -12,13 +16,13 @@ def number_generator():  # will add function
     print(f"You have selected number range {number_range1} - {number_range2}")
     # Capturing users input on guessed Number
     answer = int(input("Enter First Guess: "))
-    # Condition Checking
+    # while loop to check if the user input == to the generated number
     while answer != generated_number:
         print("Naaa That ain't it try again")
         answer = int(input("Enter First Guess: "))
         if answer == generated_number:
             break
-    print(f"Congratulations u won the number is {generated_number}")
+    print(f"Yaaay u won the number is {generated_number}")
 
 
 if __name__ == '__main__':
